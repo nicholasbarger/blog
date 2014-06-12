d0b7212cac9efb8ab3a329a6c2799bdf0bb8dd1a
Nicholas Barger <nicholas@nicholasbarger.com>
2014-06-11 23:18:52 -0400

    the updated server.js file to run in node

---
+++ server.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var port = 3000;
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nicholasbarger');
+
+// make db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+// middleware
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+// initialize routes for web
+var web = require('./routes/web.js')(app);
+
+// initialize routes for api
+var api = require('./routes/api.js')(app);
+
+// start web server
+var server = app.listen(port, function() {
+  console.log('Webserver started at http://localhost:%d/', server.address().port);
+})
+
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.send(500, { error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
