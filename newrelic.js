101477515061831b5ad2f9ef3182639f9e58184c
Nicholas Barger <nicholas@nicholasbarger.com>
2014-06-27 23:46:09 -0400

    added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['nicholasbarger-blog'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'a4c168656ac71750806237c38ac5d6bf759c2ef1',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
