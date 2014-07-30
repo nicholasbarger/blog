36a78d4efc1ee04081f407731ecac3130f6f3a95
Nicholas Barger <nicholas@nicholasbarger.com>
2014-07-29 22:59:06 -0400

    Updated home seo

--- public/controllers/latest.js
+++ public/controllers/latest.js
@@ -1,14 +1,14 @@
 nb.controller('latestController', ['$rootScope', '$sce', '$scope', 'blogFactory',
   function($rootScope, $sce, $scope, blogFactory) {
+    $rootScope.title = 'Software development and technology';
+    $rootScope.description = 'I would like to begin collecting the information that I’ve learned (and continue to learn) and provide that information here in a very straightforward way to any of those who are interested. Ideally, I hope that this small online presence will encourage others to participate in code discussion, sampling, and learning together.';
+
     $scope.post = null;
 
     (function init() {
       blogFactory.getLatest().then(function(payload) {
         $scope.post = payload;
         $scope.post.contentHtml = $sce.trustAsHtml($scope.post.content);
-
-        $rootScope.title = payload.title;
-        $rootScope.description = payload.content.replace(/<\/?[^>]+>/gi, '').substr(0, 100);
       })
     })();
   }]);
