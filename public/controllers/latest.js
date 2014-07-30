c05b15c60c918aedc350986739bce43133ed46da
Nicholas Barger <nicholas@nicholasbarger.com>
2014-07-29 22:56:55 -0400

    Added latest blog seo

--- public/controllers/latest.js
+++ public/controllers/latest.js
@@ -1,11 +1,14 @@
-nb.controller('latestController', ['$sce', '$scope', 'blogFactory',
-  function($sce, $scope, blogFactory) {
+nb.controller('latestController', ['$rootScope', '$sce', '$scope', 'blogFactory',
+  function($rootScope, $sce, $scope, blogFactory) {
     $scope.post = null;
 
     (function init() {
       blogFactory.getLatest().then(function(payload) {
         $scope.post = payload;
         $scope.post.contentHtml = $sce.trustAsHtml($scope.post.content);
+
+        $rootScope.title = payload.title;
+        $rootScope.description = payload.content.replace(/<\/?[^>]+>/gi, '').substr(0, 100);
       })
     })();
   }]);
